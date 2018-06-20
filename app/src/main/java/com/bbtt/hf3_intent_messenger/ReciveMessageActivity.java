74e3310a888d96e4f674f41169fbe8f559555229
codemass <worklette@yandex.ru>
2018-06-20 15:47:27 +0300

    В приложении 2 активити

---
+++ app/src/main/java/com/bbtt/hf3_intent_messenger/ReciveMessageActivity.java
@@ -0,0 +1,13 @@
+package com.bbtt.hf3_intent_messenger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReciveMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recive_message);
+    }
+}
