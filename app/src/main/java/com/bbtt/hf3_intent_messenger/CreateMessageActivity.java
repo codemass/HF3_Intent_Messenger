5ed15804231d07e963fd53541fde2cd923467ac4
codemass <worklette@yandex.ru>
2018-06-20 03:34:46 +0300

    1 activity complete

--- app/src/main/java/com/bbtt/hf3_intent_messenger/CreateMessageActivity.java
+++ app/src/main/java/com/bbtt/hf3_intent_messenger/CreateMessageActivity.java
@@ -2,6 +2,7 @@ package com.bbtt.hf3_intent_messenger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateMessageActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class CreateMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
+    }
+
+    //Вызвать onSendMessage() при щелчке на кнопке
+    public void onSendMessage (View view) {
+
     }
 }
